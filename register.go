f57e6af1e380219a31435984b45badaf02a207be
AndreasHGK <andreaselbergs@outlook.com>
2022-07-31 20:32:31 +0200

    register.go: Default commands

---
+++ register.go
@@ -0,0 +1,18 @@
+package df_commands
+
+import "github.com/df-mc/dragonfly/server/cmd"
+
+func RegisterAll() {
+	cmd.Register(cmd.New("gamemode", "change someone's gamemode", []string{"gm"},
+		Gamemode{},
+	))
+	cmd.Register(cmd.New("kill", "kill a player", []string{},
+		Kill{},
+	))
+	cmd.Register(cmd.New("teleport", "teleport to a destination", []string{"tp"},
+		TeleportToCoords{},
+		TeleportToPlayer{},
+		TeleportPlayerToCoords{},
+		TeleportPlayerToPlayer{},
+	))
+}
